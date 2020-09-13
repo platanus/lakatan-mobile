52c15f27138493694e8d1a1e368d6c65c5ace9f3
lfbeltran <lfbeltran@uc.cl>
2020-09-13 15:46:04 -0300

    (teams) update: style

--- App.js
+++ App.js
@@ -1,60 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Button, TextInput, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet, Text, View, Button, TextInput, KeyboardAvoidingView, TouchableOpacity, ScrollView,
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Team from './components/Team'
+import Team from './components/Team/Team';
+import color from './styles/colors';
 
-function LandingScreen ({ navigation, route }) {
-  const [teamList, setTeamList] = useState([{ id: 1, name: 'equipo 1', description: 'Descripción del mejor equipo de mobile', members: ['Javier', 'Cristobal'], state: false },
-                                            { id: 2, name: 'equipo 2', description: 'Descripción del mejor equipo de mobile', members: ['Luis F.', 'Felipe'], state: false },
-                                            { id: 3, name: 'equipo 3', description: 'best-mobile-team', members: ['javier', 'cristobal'], state: false },
-                                            { id: 4, name: 'equipo 4', description: 'best-mobile-team', members: ['javier', 'cristobal'], state: false }                                         
-                                          ])
-  const handlerTeam = (algo) =>{
-    setTeamList = (current => { [...current, algo]})
-  }
+function LandingScreen({ navigation, route }) {
+  const [teamList, setTeamList] = useState([{
+    id: 1, name: 'Mobile Capstone 1', description: 'Descripción Equipo 1...', members: ['Felipe Apablaza', 'Felipe Beltrán', 'Cristobal Ilabaca', 'Javier Tramon'], state: false,
+  },
+  {
+    id: 2, name: 'Mobile Capstone 2', description: 'Descripción Equipo 2...', members: ['Felipe Apablaza', 'Felipe Beltrán', 'Cristobal Ilabaca', 'Javier Tramon'], state: false,
+  },
+  {
+    id: 3, name: 'Mobile Capstone 3', description: 'Descripción Equipo 3...', members: ['Felipe Apablaza', 'Felipe Beltrán', 'Cristobal Ilabaca', 'Javier Tramon'], state: false,
+  },
+  {
+    id: 4, name: 'Mobile Capstone 4', description: 'Descripción Equipo 4...', members: ['Felipe Apablaza', 'Felipe Beltrán', 'Cristobal Ilabaca', 'Javier Tramon'], state: false,
+  },
+  ]);
+  const handlerTeam = (algo) => {
+    setTeamList = ((current) => { [...current, algo]; });
+  };
   return (
-    <View style={styles.container}>
-      <Button style={styles.add} title='Crear Equipo' onPress={() => navigation.navigate("Form")} />
-      {teamList.map((team) => 
-      <TouchableOpacity onPress={() => navigation.navigate("Team1", { name: team.name, description: team.description, members:team.members})}>
-        <View style={styles.team}>
-            <Text>{team.name}</Text>
+    <ScrollView>
+      <View style={styles.container}>
+
+        <View style={styles.addTeamButton}>
+          <Button title="+" color={color.white} onPress={() => navigation.navigate('Nuevo equipo')} />
         </View>
-      </TouchableOpacity>
-      )}
-    </View>
-  )
+
+        <View style={styles.listOfTeams}>
+          {teamList.map((team) => (
+            <TouchableOpacity onPress={() => navigation.navigate('Equipo', { name: team.name, description: team.description, members: team.members })}>
+              <View style={styles.teamCard}>
+                <Text style={styles.teamName}>{team.name}</Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+      </View>
+    </ScrollView>
+  );
 }
 
-function TeamsFormScreen ({ navigation }) {
-  const [name, changeName] = useState("")
-  const [description, changeDescription] = useState("")
+function TeamsFormScreen({ navigation }) {
+  const [name, changeName] = useState('');
+  const [description, changeDescription] = useState('');
   return (
-    <KeyboardAvoidingView style={styles.form} behavior="padding">
-      <View style={styles.input}>
-        <Text style={styles.tag}>Nombre:</Text>
-        <TextInput style={styles.areaInput} placeholder="Nombre" value={name} onChange={changeName} />
+    // <KeyboardAvoidingView style={styles.form} behavior="padding">
+    <View style={styles.xxxx}>
+      <View style={styles.formCard}>
+        <View style={styles.input}>
+          <Text style={styles.tag}>Nombre:</Text>
+          <TextInput style={styles.areaInput} placeholder="Ej: 'Mobile Capstone'" value={name} onChange={changeName} />
+        </View>
+        <View style={styles.input}>
+          <Text style={styles.tag}>Descripción:</Text>
+          <TextInput style={styles.areaInput} placeholder="Ej: 'Equipo encargado de Lakatan-Mobile'" value={description} onChange={changeDescription} />
+        </View>
       </View>
-      <View style={styles.input}>
-        <Text style={styles.tag}>Descripción:</Text>
-        <TextInput style={styles.areaInput} placeholder="Descripción" value={description} onChange={changeDescription} />
+
+      <View style={styles.buttonContainer}>
+        <View style={styles.cancelButton}><Button title="Cancelar" color={color.white} /></View>
+        <View style={styles.addButton}><Button title="Confirmar" color={color.white} onPress={() => navigation.navigate('Equipos', { name })} /></View>
       </View>
-      <Button title="Agregar equipo" onPress={() => navigation.navigate("Home", { name: name })} />
-    </KeyboardAvoidingView>
-  )
+    </View>
+  // </KeyboardAvoidingView>
+  );
 }
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={LandingScreen} />
-        <Stack.Screen name="Form" component={TeamsFormScreen} />
-        <Stack.Screen name="Team1" component={Team} />
+        <Stack.Screen name="Equipos" component={LandingScreen} />
+        <Stack.Screen name="Nuevo equipo" component={TeamsFormScreen} />
+        <Stack.Screen name="Equipo" component={Team} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -63,44 +91,121 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: color.white,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input:{
-    flexDirection: 'row',
-    margin: 5,
-    fontSize:20,
-  },
-  areaInput: {
-    borderWidth: 2,
-    flex: 1,
-    marginLeft: 5,
-    padding:5,
-  },
-  tag:{
-    marginTop:7,
-  },
-  form:{
-    flex: 1,
-    justifyContent: 'center', 
-    padding: 40
-  },
-  teams:{
-    flexDirection: 'row',
-    justifyContent:'center',
-  },
-  team: {shadowColor: "black",
-  shadowOffset: {width: 0, height: 2},
-  shadowRadius: 6,
-  shadowOpacity: 0.26,
-  elevation: 8,
-  backgroundColor: "white",
-  padding: 20,
-  borderRadius:10,
-  margin: 20
 
+  addTeamButton: {
+    width: '90%',
+    height: 50,
+    backgroundColor: color.blue,
+    borderRadius: 5,
+    marginTop: 15,
+    marginBottom: 10,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    fontSize: 30,
   },
-  add: {
-  }
+
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20, // REVISAR
+  },
+
+  cancelButton: {
+    // marginTop: 15,
+    width: '40%',
+    borderRadius: 5,
+    backgroundColor: color.red,
+  },
+  addButton: {
+    // marginTop: 15,
+    width: '40%',
+    borderRadius: 5,
+    backgroundColor: color.blue,
+  },
+
+  listOfTeams: {
+    width: '100%',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+
+  teamCard: {
+    width: '90%',
+    height: 170,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    justifyContent: 'center',
+    shadowColor: color.black,
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 3,
+    shadowOpacity: 0.26,
+    elevation: 8,
+    backgroundColor: color.white,
+    borderRadius: 5,
+  },
+
+  teamName: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: color.blue,
+  },
+
+  xxxx: {
+    flex: 1,
+  },
+
+  formCard: {
+
+    width: '90%',
+    // flex: 1,
+    borderRadius: 5,
+    margin: 20,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 3,
+    shadowOpacity: 0.26,
+    elevation: 8,
+    backgroundColor: color.white,
+  },
+
+  input: {
+    // flexDirection: 'row',
+    width: '90%',
+    marginVertical: 40,
+    marginHorizontal: 20,
+  },
+  areaInput: {
+    // borderWidth: 2,
+    borderBottomWidth: 0.5,
+    marginTop: 5,
+    fontSize: 16,
+    paddingTop: 5,
+    paddingBottom: 10,
+    // flex: 1,
+    // marginLeft: 5,
+    // padding: 5,
+  },
+  tag: {
+    marginTop: 7,
+    fontSize: 13,
+  },
+
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 40,
+  },
+  teams: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+
 });
