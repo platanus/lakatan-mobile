c516d8af0fdee51a2eda906b9b81840b7e9e243c
javier <jotramon@uc.cl>
2020-09-16 20:00:11 -0300

    merge

--- screens/NewTeamScreen/NewTeamScreen.js
+++ screens/NewTeamScreen/NewTeamScreen.js
@@ -7,7 +7,10 @@ import styles from './styles';
 import color from '../../styles/colors';
 
 const NewTeamScreen = (props) => {
-  const name = "equipo 1000";
+  const [name, setName] = React.useState('');
+  const [description, setDescription] = React.useState('');
+
+
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.container}>
@@ -15,17 +18,17 @@ const NewTeamScreen = (props) => {
           <Text style={styles.title}>Crear un nuevo equipo</Text>
           <View style={styles.input}>  
             <Text style={styles.tag}>Nombre:</Text>
-            <TextInput style={styles.areaInput} placeholder="Ej: 'Mobile Capstone'" />
+            <TextInput style={styles.areaInput} placeholder="Ej: 'Mobile Capstone'" value={name} onChangeText={setName} />
             <Text style={styles.tag}>Descripción:</Text>
-            <TextInput style={styles.areaInput} placeholder="Ej: 'Equipo encargado de Lakatan-Mobile'" />
+            <TextInput style={styles.areaInput} placeholder="Ej: 'Equipo encargado de Lakatan-Mobile'" value={description} onChangeText={setDescription} />
           </View>
         </View>
         <View style={styles.addUser}>
             <Button title="Agregar Usuario"  onPress={() => props.navigation.navigate('Agregar usuarios', {name})} />
         </View>
         <View style={styles.buttonContainer}>
-          <View style={styles.cancelButton}><Button title="Cancelar"  onPress={() => props.navigation.navigate('Equipos')}/></View>
-          <View style={styles.confirmButton}><Button title="Confirmar" onPress={() => props.navigation.navigate('Equipos', { name })} /></View>
+          <View style={styles.cancelButton}><Button title="Cancelar" color={color.white} onPress={() => props.navigation.navigate('Equipos')}/></View>
+          <View style={styles.confirmButton}><Button title="Confirmar" color={color.white} onPress={() => navigation.navigate('Equipos', { name })} /></View>
         </View>
       </View>
     </TouchableWithoutFeedback>
