131500eb53ad6a75d7a9de1fc5f4470c711494d3
Cristobal ilabaca <cilabaca1@uc.cl>
2020-09-16 20:33:07 -0300

    Changed map function with FlatList component

--- screens/LandingScreen/LandingScreen.js
+++ screens/LandingScreen/LandingScreen.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Text, View, Button, TouchableOpacity, ScrollView,
+  Text, View, Button, TouchableOpacity, ScrollView, FlatList,
 } from 'react-native';
 
 import styles from './styles';
 import color from '../../styles/colors';
+
+const TeamView = (props) => {
+  return (
+    <TouchableOpacity onPress={() => props.navigation.navigate('Equipo', { name: props.team.item.name, description: props.team.item.description, members: props.team.item.members })}>
+      <View style={styles.teamCard}>
+        <Text style={styles.teamName}>{props.team.item.name}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
 
 // function LandingScreen({ navigation, route }) {
 function LandingScreen(props) {
@@ -32,35 +42,31 @@ function LandingScreen(props) {
     if (props.route.params?.name) {
       const { name, description } = props.route.params
       handlerTeam({
-        id: teamId, name, description: description, members: ['Felipe apablaza'], state: false,
+        id: teamId, name, description, members: ['Felipe apablaza'], state: false,
       });
       setTeamId(teamId + 1);
     }
   }, [props.route.params?.name]);
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.addTeamButton}>
-          <Button title="+" color={color.white} onPress={() => props.navigation.navigate('Nuevo equipo')} />
-        </View>
-        <View>
-          <Button title="TEST FEEDBACK SCREEN" color={color.black} onPress={() => props.navigation.navigate('Feedback')} />
-          <Button title="TEST LOGIN SCREEN" color={color.black} onPress={() => props.navigation.navigate('Iniciar sesión')} />
-          <Button title="TEST AGREGAR USUARIO A TEAM" color={color.black} onPress={() => props.navigation.navigate('Agregar usuarios')} />
-        </View>
-        <View style={styles.listOfTeams}>
-          {teamList.map((team) => (
-            <TouchableOpacity onPress={() => props.navigation.navigate('Equipo', { name: team.name, description: team.description, members: team.members })}>
-              <View style={styles.teamCard}>
-                <Text style={styles.teamName}>{team.name}</Text>
-              </View>
-            </TouchableOpacity>
-          ))}
-        </View>
+    <View style={styles.container}>
+      <View style={styles.addTeamButton}>
+        <Button title="+" color={color.white} onPress={() => props.navigation.navigate('Nuevo equipo')} />
       </View>
-    </ScrollView>
+      <View>
+        <Button title="TEST FEEDBACK SCREEN" color={color.black} onPress={() => props.navigation.navigate('Feedback')} />
+        <Button title="TEST LOGIN SCREEN" color={color.black} onPress={() => props.navigation.navigate('Iniciar sesión')} />
+        <Button title="TEST AGREGAR USUARIO A TEAM" color={color.black} onPress={() => props.navigation.navigate('Agregar usuarios')} />
+      </View>
+      <View style={styles.listOfTeams}>
+        <FlatList
+          data={teamList}
+          renderItem={(team) => <TeamView navigation={props.navigation} team={team} />}
+          keyExtractor={(team) => team.id.toString()}
+        />
+      </View>
+    </View>
   );
-};
+}
 
 export default LandingScreen;
