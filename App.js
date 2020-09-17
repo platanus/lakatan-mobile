c4003972e6608ab0b81f49efb043b3ab2563fa57
FelipeApablaza <feapablaza@uc.cl>
2020-09-16 21:00:43 -0300

    stack error fix

--- App.js
+++ App.js
@@ -17,58 +17,21 @@ import SignUpScreen from './screens/SignUpScreen/SignUpScreen';
 
 const TeamStack = createStackNavigator();
 
-const TeamStackScreen = () => (
-  <TeamStack.Navigator screenOptions={{ headerShown: true, headerLeft: null, headerStyle: { opacity: 0, height: 50 } }}>
-    <TeamStack.Screen name="Equipos" component={LandingScreen} />
-    <TeamStack.Screen name="Equipo" component={TeamScreen} />
-    <TeamStack.Screen name="Feedback" component={FeedbackScreen} />
-    <TeamStack.Screen name="Nuevo equipo" component={NewTeamScreen} />
-    <TeamStack.Screen name="Iniciar sesión" component={LoginScreen} />
-    <TeamStack.Screen name="Agregar usuarios" component={NewUserToTeamScreen} />
-  </TeamStack.Navigator>
-);
-
-const Tab = createBottomTabNavigator();
-
-
 
 export default function App() {
-
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Equipos"
-          component={TeamStackScreen}
-          options={{
-            title: 'Equipos',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="ios-contacts" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Perfil"
-          component={TeamStackScreen}
-          options={{
-            title: 'Perfil',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="ios-contact" color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-      <Stack.Navigator>
-              <Stack.Screen name ="Splash" component={SplashScreen} options={{ headerShown: false }}/>
-              <Stack.Screen name ="SignIn" component={SignInScreen} options={{ headerShown: false }} />
-              <Stack.Screen name ="SignUp" component={SignUpScreen} options={{ headerShown: false }}/>
-              <Stack.Screen name="Equipos" component={LandingScreen} options={{ gestureEnabled: false ,headerLeft: null }} />
-              <Stack.Screen name="Equipo" component={TeamScreen} />
-              <Stack.Screen name="Feedback" component={FeedbackScreen} />
-              <Stack.Screen name="Nuevo equipo" component={NewTeamScreen} />
-              <Stack.Screen name="Iniciar sesión" component={LoginScreen} />
-              <Stack.Screen name="Agregar usuarios" component={NewUserToTeamScreen} />
-      </Stack.Navigator>
+      <TeamStack.Navigator>
+        <TeamStack.Screen name ="Splash" component={SplashScreen} options={{ headerShown: false }}/>
+        <TeamStack.Screen name ="SignIn" component={SignInScreen} options={{ headerShown: false }} />
+        <TeamStack.Screen name ="SignUp" component={SignUpScreen} options={{ headerShown: false }}/>
+        <TeamStack.Screen name="Equipos" component={LandingScreen} options={{ gestureEnabled: false ,headerLeft: null }} />
+        <TeamStack.Screen name="Equipo" component={TeamScreen} />
+        <TeamStack.Screen name="Feedback" component={FeedbackScreen} />
+        <TeamStack.Screen name="Nuevo equipo" component={NewTeamScreen} />
+        <TeamStack.Screen name="Iniciar sesión" component={LoginScreen} />
+        <TeamStack.Screen name="Agregar usuarios" component={NewUserToTeamScreen} />
+      </TeamStack.Navigator>
     </NavigationContainer>
   );
 }
