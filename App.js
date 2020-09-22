f17e130872edd5967ace7dc09d2379b6059fb3a8
FelipeApablaza <feapablaza@uc.cl>
2020-09-22 03:31:19 -0300

    persist config added

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-filename-extension */
 import React, {useState} from 'react';
 import { Provider } from 'react-redux'
-import { store } from './store';
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -23,19 +24,21 @@ const TeamStack = createStackNavigator();
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <TeamStack.Navigator>
-          <TeamStack.Screen name ="Splash" component={SplashScreen} options={{ headerShown: false }}/>
-          <TeamStack.Screen name ="SignIn" component={SignInScreen} options={{ headerShown: false }} />
-          <TeamStack.Screen name ="SignUp" component={SignUpScreen} options={{ headerShown: false }}/>
-          <TeamStack.Screen name="Equipos" component={LandingScreen} options={{ gestureEnabled: false ,headerLeft: null }} />
-          <TeamStack.Screen name="Equipo" component={TeamScreen} />
-          <TeamStack.Screen name="Feedback" component={FeedbackScreen} />
-          <TeamStack.Screen name="Nuevo equipo" component={NewTeamScreen} />
-          <TeamStack.Screen name="Iniciar sesión" component={LoginScreen} />
-          <TeamStack.Screen name="Agregar usuarios" component={NewUserToTeamScreen} />
-        </TeamStack.Navigator>
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}> 
+        <NavigationContainer>
+          <TeamStack.Navigator>
+            <TeamStack.Screen name ="Splash" component={SplashScreen} options={{ headerShown: false }}/>
+            <TeamStack.Screen name ="SignIn" component={SignInScreen} options={{ headerShown: false }} />
+            <TeamStack.Screen name ="SignUp" component={SignUpScreen} options={{ headerShown: false }}/>
+            <TeamStack.Screen name="Equipos" component={LandingScreen} options={{ gestureEnabled: false ,headerLeft: null }} />
+            <TeamStack.Screen name="Equipo" component={TeamScreen} />
+            <TeamStack.Screen name="Feedback" component={FeedbackScreen} />
+            <TeamStack.Screen name="Nuevo equipo" component={NewTeamScreen} />
+            <TeamStack.Screen name="Iniciar sesión" component={LoginScreen} />
+            <TeamStack.Screen name="Agregar usuarios" component={NewUserToTeamScreen} />
+          </TeamStack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- store/index.js
+++ store/index.js
@@ -1,10 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit"
+import AsyncStorage from '@react-native-community/async-storage'
+import createSagaMiddleware from 'redux-saga';
+import { configureStore } from "@reduxjs/toolkit" 
+import { persistStore, persistReducer } from 'redux-persist'
 import rootReducer from './reducers'
 
+const sagaMiddleware = createSagaMiddleware(); // crear el middleware de saga para eventos asíncronos
 
-const store = configureStore({
-    reducer: rootReducer
+const persistConfig = {         // envuelve el AsyncStorage (memoria local) en el persistor
+    key: 'root',
+    storage: AsyncStorage,
+    whitelist: ['authentication'] // persistirán solo el estado del reducir auth...
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer) // envuelve el reducer en el persistor
+
+const store = configureStore({     // se crea la store y se le entrega el reducer
+    reducer: persistedReducer,
+    middleware: [sagaMiddleware]
 })
 
+const persistor = persistStore(store)  // permite generar un delay hasta que los datos son obtenidos de la store
+                                        // sirve para implementar la splash screen
 
-export {store}
+export {store, persistor}
