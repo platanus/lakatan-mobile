1fcb49ebcceeaf1e4773467bd94da07ee64b597e
lfbeltran <lfbeltran@uc.cl>
2020-10-01 00:50:20 -0300

    (team) update: new user buttons style

--- screens/NewUserToTeamScreen/NewUserToTeamScreen.js
+++ screens/NewUserToTeamScreen/NewUserToTeamScreen.js
@@ -2,11 +2,10 @@ import ListItem from 'antd-mobile/lib/list/ListItem';
 /* eslint-disable react/jsx-filename-extension */
 import React, { useState } from 'react';
 import {
-  View, Text, Button, TextInput, TouchableWithoutFeedback, Keyboard,
+  View, Text, Button, TextInput, TouchableWithoutFeedback, Keyboard, TouchableOpacity,
 } from 'react-native';
 
 import MultiSelect from 'react-native-multiple-select';
-
 
 import color from '../../styles/colors';
 import styles from '../../styles/NewUserToTeamScreen/NewUserToTeamScreen';
@@ -26,40 +25,47 @@ const users = [{
 }];
 
 const NewUserToTeamScreen = (props) => {
-    
-  const {members} = props.route.params;
-  const [selectedItems, setSelectedItems] = useState(members)
+  const { members } = props.route.params;
+  const [selectedItems, setSelectedItems] = useState(members);
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.container}>
-        <View >
+        <View>
           <MultiSelect
             items={users}
             uniqueKey="id"
-            alwaysShowSelectText={true}
+            alwaysShowSelectText
             onSelectedItemsChange={setSelectedItems}
             selectedItems={selectedItems}
             colors={{ primary: color.blue, success: color.blue, text: color.black }}
             confirmText="Confirmar"
             selectText="Elige usuarios"
             searchInputPlaceholderText="Elige un usuario a agregar..."
-            tagRemoveIconColor="#CCC"
-            tagBorderColor="#CCC"
+            tagRemoveIconColor={color.softGray}
+            tagBorderColor={color.softGray}
             tagTextColor={color.black}
-            selectedItemTextColor= {color.blue}
-            selectedItemIconColor="#CCC"
-            itemTextColor="#000"
+            selectedItemTextColor={color.blue}
+            selectedItemIconColor={color.softGray}
+            itemTextColor={color.black}
             displayKey="name"
-            searchInputStyle={{ color: '#CCC' }}
+            searchInputStyle={{ color: color.softGray }}
             submitButtonColor={color.blue}
             submitButtonText="Submit"
             button="40"
           />
         </View>
         <View style={styles.buttonContainer}>
-          <View style={styles.cancelButton}><Button title="Cancelar"  onPress={() =>  props.navigation.navigate('Nuevo equipo', { name: props.route.params.name ,members: members})} /></View>
-          <View style={styles.confirmButton}><Button title="Confirmar" onPress={() => props.navigation.navigate('Nuevo equipo', { name: props.route.params.name ,members: selectedItems})} /></View>
+          <View style={styles.cancelButton}>
+            <TouchableOpacity onPress={() => props.navigation.navigate('Nuevo equipo', { name: props.route.params.name, members })}>
+              <Text style={styles.textCancelButton}>Cancelar</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.confirmButton}>
+            <TouchableOpacity onPress={() => props.navigation.navigate('Nuevo equipo', { name: props.route.params.name, members: selectedItems })}>
+              <Text style={styles.textConfirmButton}>Confirmar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </TouchableWithoutFeedback>
--- styles/colors.js
+++ styles/colors.js
@@ -4,6 +4,7 @@ export default {
   yellow: '#fac537',
   red: '#ff4d4f',
   gray: '#bfbfbf',
+  softGray: '#CCC',
   white: '#ffffff',
   black: '#000000',
 };
