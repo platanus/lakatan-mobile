0a2c210e6b829398f44804391005f22e99c8cc3f
lfbeltran <lfbeltran@uc.cl>
2020-10-03 00:07:10 -0300

    (teams) fix: principal view

--- screens/LandingScreen/LandingScreen.js
+++ screens/LandingScreen/LandingScreen.js
@@ -1,35 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Text, View, Button, TouchableOpacity, ScrollView, FlatList } from 'react-native';
+  Text, View, Button, TouchableOpacity, ScrollView, FlatList,
+} from 'react-native';
 
 import styles from '../../styles/LandingScreen/LandingScreen';
 import color from '../../styles/colors';
 
 const TeamView = (props) => {
-  const {name, description, members} = props.team.item;
+  const { name, description, members } = props.team.item;
 
- 
   return (
-    <TouchableOpacity onPress={() => props.navigation.navigate('Team', { name: name, description: description, members: members })}>
+    <TouchableOpacity onPress={() => props.navigation.navigate('Team', { name, description, members })}>
       <View style={styles.teamCard}>
         <Text style={styles.teamName}>{props.team.item.name}</Text>
       </View>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 function LandingScreen(props) {
   const [teamList, setTeamList] = useState([{
-    id: 1, name: 'Mobile Capstone 1', description: 'Propósito Equipo 1...', members: [{id: 1, name: 'Felipe Apablaza'},{id: 2, name: 'Felipe Beltrán'},{id: 3, name: 'Cristobal Ilabaca'},{id: 4, name: 'Javier Tramon'}], state: false,
+    id: 1, name: 'Mobile Capstone 1', description: 'Propósito Equipo 1...', members: [{ id: 1, name: 'Felipe Apablaza' }, { id: 2, name: 'Felipe Beltrán' }, { id: 3, name: 'Cristobal Ilabaca' }, { id: 4, name: 'Javier Tramon' }], state: false,
   },
   {
-    id: 2, name: 'Mobile Capstone 2', description: 'Propósito Equipo 2...', members: [{id: 1, name: 'Felipe Apablaza'},{id: 2, name: 'Felipe Beltrán'},{id: 3, name: 'Cristobal Ilabaca'},{id: 4, name: 'Javier Tramon'}], state: false,
+    id: 2, name: 'Mobile Capstone 2', description: 'Propósito Equipo 2...', members: [{ id: 1, name: 'Felipe Apablaza' }, { id: 2, name: 'Felipe Beltrán' }, { id: 3, name: 'Cristobal Ilabaca' }, { id: 4, name: 'Javier Tramon' }], state: false,
   },
   {
-    id: 3, name: 'Mobile Capstone 3', description: 'Propósito Equipo 3...', members: [{id: 1, name: 'Felipe Apablaza'},{id: 2, name: 'Felipe Beltrán'},{id: 3, name: 'Cristobal Ilabaca'},{id: 4, name: 'Javier Tramon'}], state: false,
+    id: 3, name: 'Mobile Capstone 3', description: 'Propósito Equipo 3...', members: [{ id: 1, name: 'Felipe Apablaza' }, { id: 2, name: 'Felipe Beltrán' }, { id: 3, name: 'Cristobal Ilabaca' }, { id: 4, name: 'Javier Tramon' }], state: false,
   },
   {
-    id: 4, name: 'Mobile Capstone 4', description: 'Propósito Equipo 4...', members: [{id: 1, name: 'Felipe Apablaza'},{id: 2, name: 'Felipe Beltrán'},{id: 3, name: 'Cristobal Ilabaca'},{id: 4, name: 'Javier Tramon'}], state: false,
+    id: 4, name: 'Mobile Capstone 4', description: 'Propósito Equipo 4...', members: [{ id: 1, name: 'Felipe Apablaza' }, { id: 2, name: 'Felipe Beltrán' }, { id: 3, name: 'Cristobal Ilabaca' }, { id: 4, name: 'Javier Tramon' }], state: false,
   },
   ]);
 
@@ -41,9 +41,9 @@ function LandingScreen(props) {
 
   useEffect(() => {
     if (props.route.params?.name) {
-      const { name, description, members} = props.route.params
+      const { name, description, members } = props.route.params;
       handlerTeam({
-        id: teamId, name, description, members: members, state: false,
+        id: teamId, name, description, members, state: false,
       });
       setTeamId(teamId + 1);
     }
