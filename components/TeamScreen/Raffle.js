ef0a9034c49ddeef1f5fe052247e12fb630c1dac
lfbeltran <lfbeltran@uc.cl>
2020-10-05 23:40:59 -0300

    (rite) update: few users selected

--- components/TeamScreen/Raffle.js
+++ components/TeamScreen/Raffle.js
@@ -8,13 +8,29 @@ import { bounceInUp, bounceInDown } from 'react-native-animatable';
 import color from '../../styles/colors';
 import styles from '../../styles/TeamScreen/TeamScreen';
 
+import TeamList from '../../components/TeamScreen/TeamList';
+
+const selectedUsersId = [5, 6, 8]; // TO DO: estos son los seleccionados que llegan de backend
+
+let fewUsers = false;
+
 const Raffle = ({
   users, setVisible, visible, navigation,
 }) => {
-  const [userSelected, setUserSelected] = useState('');
+  const [userSelected, setUserSelected] = useState([]);
   useEffect(() => {
     const random = Math.floor(Math.random() * users.length);
-    setUserSelected(users[random].email);
+    // setUserSelected(users[random].email);
+    setUserSelected(users.filter((user) => selectedUsersId.includes(user.id)));
+    if (selectedUsersId.length > 1) {
+      fewUsers = true;
+    }
+    console.log("HOLA");
+    console.log(users);
+    console.log(selectedUsersId);
+    console.log("--");
+    console.log(users.filter((user) => selectedUsersId.includes(user.id)));
+    console.log("CHAO");
   }, []);
 
   const toggleModalOff = () => {
@@ -30,8 +46,15 @@ const Raffle = ({
         style={styles.modal}
       >
         <View style={styles.modalView}>
-          <Text style={styles.modalMessage}>El usuario seleccionado es:</Text>
-          <Text style={styles.modalUser}>{userSelected}</Text>
+          {!fewUsers ? (
+            <Text style={styles.modalMessage}>El usuario seleccionado es:</Text>
+          ) : (
+            <Text style={styles.modalMessage}>Los usuarios seleccionados son:</Text>
+          )}
+          {/* <Text style={styles.modalUser}>{userSelected}</Text> */}
+          <View style={styles.teamListContainer}>
+            <TeamList users={userSelected} inUserList={false} />
+          </View>
           <Emoji name=":tada:" style={styles.modalEmoji} />
           <View style={styles.exitButtonContainer}>
             <TouchableOpacity style={styles.exitButton} onPress={toggleModalOff}>
