a984f7ec847d2ac5c73974abadbdc6d6ebb21900
Cristobal ilabaca <cilabaca1@uc.cl>
2020-11-11 21:46:33 -0300

    feat(slack-auth) Updated the scope to write in slack

--- components/Slack/slack_auth.js
+++ components/Slack/slack_auth.js
@@ -8,7 +8,7 @@ import styles from '../../styles/SlackAuth/SlackAuth';
 
 const SlackAuth = () => {
   const client = clientID;
-  const link = `https://slack.com/oauth/v2/authorize?scope=channels:read,groups:read,team:read,usergroups:read,users.profile:read,users:read,users:read.email,chat:write.public,chat:write:bot,chat:write:user,chat:write&client_id=${client}`;
+  const link = `https://slack.com/oauth/v2/authorize?scope=channels:read,groups:read,team:read,usergroups:read,users.profile:read,users:read,users:read.email,chat:write,chat:write.public&client_id=${client}`;
 
   return (
     <TouchableOpacity style={styles.button} onPress={() => Linking.openURL(link)}>
