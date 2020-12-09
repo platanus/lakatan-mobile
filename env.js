161afc2c3ee4441d0a8a9c41b7e2df71d18fa743
javier <jotramon@uc.cl>
2020-12-08 21:33:25 -0300

    fix(bucketeer): setting env variable to access in bucketeer repo

--- env.js
+++ env.js
@@ -1,6 +1,7 @@
 const stagingUrl = 'https://pl-lakatan-production.herokuapp.com/';
 const clientID = `1414886919431.1429629233266`;
+const BUCKETEER_BUCKET_NAME = 'bucketeer-311c604a-075e-42b3-ba72-18c6b5d17e65';
 
 export default stagingUrl;
-export { clientID };
+export { clientID, BUCKETEER_BUCKET_NAME};
     // this env file is not setted yet.
